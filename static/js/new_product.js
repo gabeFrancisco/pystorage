4b9774e4ddcd3eaf14a3f82b238d58140233c4bc
gabefrancisco <gabegit2@gmail.com>
2026-02-25 21:41:23 -0300

    created new_product.js an attached to new_product.html

---
+++ static/js/new_product.js
@@ -0,0 +1,33 @@
+const newProductForm = document.getElementById('newProductForm')
+
+const _name = document.getElementById('name')
+const description = document.getElementById('description')
+const quantity = document.getElementById('quantity')
+const price = document.getElementById('price')
+const categoryId = document.getElementById('category')
+
+newProductForm.addEventListener('submit', (event) => {
+    if (_name.value.trim() == "") {
+        alert("Nome obrigatório!")
+        event.preventDefault()
+        return
+    }
+
+    if (description.value.trim() == "") {
+        alert("Descrição é obrigatória!")
+        event.preventDefault()
+        return
+    }
+
+    if (quantity.value.trim() == "") {
+        alert("Quantidade deve ser maior que 0!")
+        event.preventDefault()
+        return
+    }
+
+    if (price.value.trim() == "") {
+        alert("Preço deve ser maior que 0!")
+        event.preventDefault()
+        return
+    }
+})
