936c9689326f73c1b6b69f51521ce28a9eed9f9a
gabefrancisco <gabegit2@gmail.com>
2026-03-07 22:55:10 -0300

    removed info_repository

--- controllers/home.py
+++ controllers/home.py
@@ -1,12 +1,8 @@
 from flask import Blueprint, render_template, request
-from repositories.info_repository import InfoRepository
 
 home_bp = Blueprint("home", __name__)
-
-infoRepository = InfoRepository()
 
 
 @home_bp.route("/")
 def index():
-    dbSize = infoRepository.getDbSize()
-    return render_template("index.html", dbSize=dbSize)
+    return render_template("index.html")
